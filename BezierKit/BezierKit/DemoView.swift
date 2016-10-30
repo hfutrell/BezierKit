2ec87c62bec93e01a051faed585b80b105adcaf9
Holmes Futrell <holmes@gingerlabs.com>
2016-10-29 20:19:04 -0700

    fixed things so that custom class actually used (had to specify module).

--- BezierKit/BezierKit/DemoView.swift
+++ BezierKit/BezierKit/DemoView.swift
@@ -6,4 +6,24 @@
 //  Copyright © 2016 Holmes Futrell. All rights reserved.
 //
 
-import Foundation
+import AppKit
+
+class DemoView: NSView {
+    
+    required init?(coder: NSCoder) {
+        NSLog("init with coder")
+        super.init(coder: coder)
+    }
+        
+    override func draw(_ dirtyRect: NSRect) {
+
+        NSLog("draw")
+        
+        let context: CGContext = NSGraphicsContext.current()!.cgContext
+        
+        context.setFillColor(NSColor.red.cgColor)
+        context.fill(self.bounds)
+        
+    }
+    
+}
