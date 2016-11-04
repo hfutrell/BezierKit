512e5b157c6c4d59ebc16c82abe404e2cd59617c
Holmes Futrell <holmes@gingerlabs.com>
2016-11-04 00:25:54 -0700

    start of utils.

--- BezierKit/BezierKit/Utils.swift
+++ BezierKit/BezierKit/Utils.swift
@@ -7,3 +7,34 @@
 //
 
 import Foundation
+
+class Utils {
+    
+    static func abcRatio(n: Int, t: CGFloat = 0.5) -> BKFloat {
+        // see ratio(t) note on http://pomax.github.io/bezierinfo/#abc
+        assert(n == 2 || n == 3)
+        if ( t == 0 || t == 1) {
+            return t
+        }
+        let bottom = pow(t, CGFloat(n)) + pow(1 - t, CGFloat(n))
+        let top = bottom - 1
+        return abs(top/bottom);
+    }
+    
+    static func projectionRatio(n: Int, t: CGFloat = 0.5) -> BKFloat {
+        // see u(t) note on http://pomax.github.io/bezierinfo/#abc
+        assert(n == 2 || n == 3)
+        if (t == 0 || t == 1) {
+            return t
+        }
+        let top = pow(1.0 - t, CGFloat(n))
+        let bottom = pow(t, CGFloat(n)) + top
+        return top/bottom;
+
+    }
+    
+    static func dist(_ p1: BKPoint,_ p2: BKPoint) -> BKFloat {
+        return (p1 - p2).length
+    }
+    
+}
