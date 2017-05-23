1aaeb84c24fb8aa37dff5a611d040bc046a236df
Holmes Futrell <holmes@gingerlabs.com>
2017-05-22 17:27:03 -0700

    patched BezierCurve so it would not fail when intersected against lines, and lines would call the proper implementation.

--- BezierKit/Library/BezierCurve.swift
+++ BezierKit/Library/BezierCurve.swift
@@ -463,6 +463,9 @@ extension BezierCurve {
         else if let q = curve as? QuadraticBezierCurve {
             return Self.internalCurvesIntersect(c1: [s], c2: [Subcurve(curve: q)], curveIntersectionThreshold: curveIntersectionThreshold)
         }
+        else if let l = curve as? LineSegment {
+            return self.intersects(line: l)
+        }
         else {
             fatalError("unsupported")
         }
@@ -596,4 +599,5 @@ public protocol BezierCurve {
     func length() -> BKFloat
     func extrema() -> (xyz: [[BKFloat]], values: [BKFloat] )
     func generateLookupTable(withSteps steps: Int) -> [BKPoint]
+    func intersects(curve: BezierCurve, curveIntersectionThreshold: BKFloat) -> [Intersection]
 }
