2a9f542ab56108a79b28f79d1c7d6e82008e34a1
Holmes Futrell <holmes@gingerlabs.com>
2017-08-05 12:44:53 -0700

    disabled placeholder tests, moved a useful utility function into file.

--- BezierKit/BezierKitTests/BezierKitTests.swift
+++ BezierKit/BezierKitTests/BezierKitTests.swift
@@ -11,6 +11,17 @@ import XCTest
 
 class BezierKitTests: XCTestCase {
     
+    static internal func intersections(_ intersections: [Intersection], betweenCurve c1: BezierCurve, andOtherCurve c2: BezierCurve, areWithinTolerance epsilon: BKFloat) -> Bool {
+        for i in intersections {
+            let p1 = c1.compute(i.t1)
+            let p2 = c2.compute(i.t2)
+            if (p1 - p2).length > epsilon {
+                return false
+            }
+        }
+        return true
+    }
+    
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -21,16 +32,16 @@ class BezierKitTests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
+//    func testExample() {
+//        // This is an example of a functional test case.
+//        // Use XCTAssert and related functions to verify your tests produce the correct results.
+//    }
+//    
+//    func testPerformanceExample() {
+//        // This is an example of a performance test case.
+//        self.measure {
+//            // Put the code you want to measure the time of here.
+//        }
+//    }
     
 }
