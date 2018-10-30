c8b9b61c96b06051f65fc7fe3500c03d3e34ec68
Holmes Futrell <holmes@gingerlabs.com>
2018-10-29 17:14:57 -0700

    added a note about a mouse position that would cause a graphical glitch.

--- BezierKit/MacDemos/Demos.swift
+++ BezierKit/MacDemos/Demos.swift
@@ -433,6 +433,9 @@ class Demos {
                                 if let mouse = demoState.lastInputLocation {
                                     
                                     //let m2 = CGPoint(x: -21.19140625, y: 131.38671875)
+                                    //let me2 = CGPoint(x: 24.34375, y: 110.703125)
+                                    
+                                    //let me3 = CGPoint(x: -7.78515625, y: 161.7265625) // seems to cause an issue because intersections[5].t = 0.99999422905833845
                                     
                                     var translation = CGAffineTransform.init(translationX: mouse.x, y: mouse.y)
                                     let cgPath2: CGPath = CTFontCreatePathForGlyph(font, glyph2, &translation)!
