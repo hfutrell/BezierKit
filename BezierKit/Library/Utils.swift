de4131eedb292232df124b714db30f3ca18a37fd
Holmes Futrell <holmes@gingerlabs.com>
2018-11-28 13:23:35 -0800

    rudimentary bailout code for cases we cannot handle appropriately to avoid infinite loops.

--- BezierKit/Library/Utils.swift
+++ BezierKit/Library/Utils.swift
@@ -382,6 +382,12 @@ internal class Utils {
     static func pairiteration<C1, C2>(_ c1: Subcurve<C1>, _ c2: Subcurve<C2>, _ results: inout [Intersection], _ threshold: CGFloat = BezierKit.defaultIntersectionThreshold) {
         let c1b = c1.curve.boundingBox
         let c2b = c2.curve.boundingBox
+        
+        if results.count > 20 {
+            // TODO: better bailout conditions
+            return
+        }
+        
         if c1b.overlaps(c2b) == false {
             return
         }
