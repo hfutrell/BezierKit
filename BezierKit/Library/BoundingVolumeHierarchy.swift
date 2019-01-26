52516572469b3f52efe6fbb450775bdb5e792f48
Holmes Futrell <holmes@gingerlabs.com>
2019-01-25 20:36:22 -0800

    re-order internal nodes.

--- BezierKit/Library/BoundingVolumeHierarchy.swift
+++ BezierKit/Library/BoundingVolumeHierarchy.swift
@@ -12,6 +12,8 @@ internal class BVH {
     
     fileprivate let boundingBoxes: UnsafePointer<BoundingBox>?
     fileprivate var inodeCount: Int
+    fileprivate var lastRowIndex: Int
+    fileprivate var elementCount: Int
     
     fileprivate var root: BVHNode {
         return BVHNode(index: 0, bvh: Unmanaged.passUnretained(self) )
@@ -26,12 +28,36 @@ internal class BVH {
         }
     }
     
+    fileprivate static func leafNodeIndexToElementIndex(_ nodeIndex: Int, leafCount: Int, lastRowIndex: Int) -> Int {
+        assert(nodeIndex + 1 >= leafCount, "not actually a leaf (index of left child is a valid node")
+        var elementIndex = nodeIndex - lastRowIndex
+        if elementIndex < 0 {
+            elementIndex += leafCount
+        }
+        return elementIndex
+    }
+    
     init(boxes leafBoxes: [BoundingBox]) {
         // create a complete binary tree of bounding boxes where boxes[0] is the root and left child is 2*index+1 and right child is 2*index+2
         let boxes = UnsafeMutablePointer<BoundingBox>.allocate(capacity: 2*leafBoxes.count-1)
+        
+
+        self.elementCount = leafBoxes.count
+        
         self.inodeCount = leafBoxes.count-1
+        
+        var lastRowIndex = 0
+        while lastRowIndex < self.inodeCount {
+            lastRowIndex *= 2
+            lastRowIndex += 1
+        }
+        self.lastRowIndex = lastRowIndex
+
+        
         for i in 0..<leafBoxes.count {
-            boxes[i+self.inodeCount] = leafBoxes[i]
+            let nodeIndex = i+self.inodeCount
+            let elementIndex = BVH.leafNodeIndexToElementIndex(nodeIndex, leafCount: leafBoxes.count, lastRowIndex: lastRowIndex)
+            boxes[nodeIndex] = leafBoxes[elementIndex]
         }
         for i in stride(from: self.inodeCount-1, through: 0, by: -1) {
             boxes[i] = BoundingBox(first: boxes[2*i+1], second: boxes[2*i+2])
@@ -57,7 +83,8 @@ internal class BVH {
         }
         func intersects(index: Int, callback: (Int, Int) -> Void) {
             if index >= inodecount { // if it's a leaf node
-                callback(index - inodecount, index - inodecount)
+                let elementIndex1 = BVH.leafNodeIndexToElementIndex(index, leafCount: self.elementCount, lastRowIndex: lastRowIndex)
+                callback(elementIndex1, elementIndex1)
             }
             else {
                 let l = 2*index+1
@@ -74,12 +101,9 @@ internal class BVH {
             let leaf1 = index1 >= inodecount
             let leaf2 = index2 >= inodecount
             if leaf1, leaf2 {
-                if index1 < index2 {
-                    callback(index1 - inodecount, index2 - inodecount)
-                }
-                else {
-                    callback(index2 - inodecount, index1 - inodecount)
-                }
+                let elementIndex1 = BVH.leafNodeIndexToElementIndex(index1, leafCount: self.elementCount, lastRowIndex: lastRowIndex)
+                let elementIndex2 = BVH.leafNodeIndexToElementIndex(index2, leafCount: self.elementCount, lastRowIndex: lastRowIndex)
+                callback(elementIndex1, elementIndex2)
             }
             else if leaf1 {
                 intersects(index1: index1, index2: 2*index2+1, callback: callback)
@@ -112,7 +136,9 @@ internal class BVH {
             let leaf1 = index1 >= inodecount1
             let leaf2 = index2 >= inodecount2
             if leaf1, leaf2 {
-                callback(index1 - inodecount1, index2 - inodecount2)
+                let elementIndex1 = BVH.leafNodeIndexToElementIndex(index1, leafCount: self.elementCount, lastRowIndex: self.lastRowIndex)
+                let elementIndex2 = BVH.leafNodeIndexToElementIndex(index2, leafCount: other.elementCount, lastRowIndex: other.lastRowIndex)
+                callback(elementIndex1, elementIndex2)
             }
             else if leaf1 {
                 intersects(index1: index1, index2: 2*index2+1, callback: callback)
@@ -153,7 +179,7 @@ internal struct BVHNode {
             return .internal
         }
         else {
-            return .leaf(elementIndex: index - internalNodeCount)
+            return .leaf(elementIndex: BVH.leafNodeIndexToElementIndex(index, leafCount: self.bvh.takeUnretainedValue().elementCount, lastRowIndex: self.bvh.takeUnretainedValue().lastRowIndex ))
         }
     }
     
