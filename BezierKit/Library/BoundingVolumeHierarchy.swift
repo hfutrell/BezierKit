e6ffa568faf130baad5ed0e1cb53f779571b4c0e
Holmes Futrell <holmes@gingerlabs.com>
2019-01-25 21:07:49 -0800

    slim down BVHNode implementation.

--- BezierKit/Library/BoundingVolumeHierarchy.swift
+++ BezierKit/Library/BoundingVolumeHierarchy.swift
@@ -10,22 +10,13 @@ import CoreGraphics
 
 internal class BVH {
     
-    fileprivate let boundingBoxes: UnsafePointer<BoundingBox>?
+    fileprivate let boundingBoxes: UnsafePointer<BoundingBox>
     fileprivate var inodeCount: Int
     fileprivate var lastRowIndex: Int
     fileprivate var elementCount: Int
-    
-    fileprivate var root: BVHNode {
-        return BVHNode(index: 0, bvh: Unmanaged.passUnretained(self) )
-    }
-    
+        
     var boundingBox: BoundingBox {
-        if let boxes = self.boundingBoxes {
-            return boxes[0]
-        }
-        else {
-            return BoundingBox.empty
-        }
+        return self.boundingBoxes[0]
     }
     
     fileprivate static func leafNodeIndexToElementIndex(_ nodeIndex: Int, leafCount: Int, lastRowIndex: Int) -> Int {
@@ -39,11 +30,10 @@ internal class BVH {
     
     init(boxes leafBoxes: [BoundingBox]) {
         // create a complete binary tree of bounding boxes where boxes[0] is the root and left child is 2*index+1 and right child is 2*index+2
+        assert(leafBoxes.count > 0)
         let boxes = UnsafeMutablePointer<BoundingBox>.allocate(capacity: 2*leafBoxes.count-1)
         
-
         self.elementCount = leafBoxes.count
-        
         self.inodeCount = leafBoxes.count-1
         
         var lastRowIndex = 0
@@ -52,7 +42,6 @@ internal class BVH {
             lastRowIndex += 1
         }
         self.lastRowIndex = lastRowIndex
-
         
         for i in 0..<leafBoxes.count {
             let nodeIndex = i+self.inodeCount
@@ -66,21 +55,27 @@ internal class BVH {
     }
     
     deinit {
-        self.boundingBoxes?.deallocate()
+        self.boundingBoxes.deallocate()
     }
     
     func visit(callback: (BVHNode, Int) -> Bool) {
-        guard self.boundingBoxes != nil else {
-            return
+        func visit(index: Int, depth: Int, callback: (BVHNode, Int) -> Bool) {
+            guard callback(BVHNode(boundingBox: self.boundingBoxes[index],
+                                   type: index < self.inodeCount ? .internal: .leaf(elementIndex: BVH.leafNodeIndexToElementIndex(index, leafCount: self.elementCount, lastRowIndex: self.lastRowIndex))),
+                           depth) == true else {
+                return
+            }
+            if index < self.inodeCount {
+                let nextDepth = depth + 1
+                visit(index: 2*index+1, depth: nextDepth, callback: callback)
+                visit(index: 2*index+2, depth: nextDepth, callback: callback)
+            }
         }
-        root.visit(callback: callback)
+        visit(index: 0, depth: 0, callback: callback)
     }
     
     func intersects(callback: (Int, Int) -> Void) {
         let inodecount = self.inodeCount
-        guard let boxes = self.boundingBoxes else {
-            return
-        }
         func intersects(index: Int, callback: (Int, Int) -> Void) {
             if index >= inodecount { // if it's a leaf node
                 let elementIndex1 = BVH.leafNodeIndexToElementIndex(index, leafCount: self.elementCount, lastRowIndex: lastRowIndex)
@@ -95,7 +90,7 @@ internal class BVH {
             }
         }
         func intersects(index1: Int, index2: Int, callback: (Int, Int) -> Void) {
-            guard boxes[index1].overlaps(boxes[index2]) else {
+            guard self.boundingBoxes[index1].overlaps(self.boundingBoxes[index2]) else {
                 return // nothing to do
             }
             let leaf1 = index1 >= inodecount
@@ -126,9 +121,8 @@ internal class BVH {
     func intersects(node other: BVH, callback: (Int, Int) -> Void) {
         let inodecount1 = self.inodeCount
         let inodecount2 = other.inodeCount
-        guard let boxes1 = self.boundingBoxes, let boxes2 = other.boundingBoxes else {
-            return
-        }
+        let boxes1 = self.boundingBoxes
+        let boxes2 = other.boundingBoxes
         func intersects(index1: Int, index2: Int, callback: (Int, Int) -> Void) {
             guard boxes1[index1].overlaps(boxes2[index2]) else {
                 return // nothing to do
@@ -160,55 +154,14 @@ internal class BVH {
 }
 
 internal struct BVHNode {
-    
-    private let bvh: Unmanaged<BVH>
-    fileprivate let index: Int
-    
-    fileprivate init(index: Int, bvh: Unmanaged<BVH>) {
-        self.bvh = bvh
-        self.index = index
-    }
-    
-    internal var boundingBox: BoundingBox {
-        return self.bvh.takeUnretainedValue().boundingBoxes![index]
-    }
-    
-    internal var nodeType: NodeType {
-        let internalNodeCount = self.bvh.takeUnretainedValue().inodeCount
-        if index < internalNodeCount {
-            return .internal
-        }
-        else {
-            return .leaf(elementIndex: BVH.leafNodeIndexToElementIndex(index, leafCount: self.bvh.takeUnretainedValue().elementCount, lastRowIndex: self.bvh.takeUnretainedValue().lastRowIndex ))
-        }
-    }
-    
-    internal enum NodeType {
+    var boundingBox: BoundingBox
+    var nodeType: NodeType
+    enum NodeType {
         case leaf(elementIndex: Int)
         case `internal`
     }
-    
-    fileprivate var left: BVHNode {
-        return BVHNode(index: 2 * self.index + 1, bvh: self.bvh)
-    }
-    
-    fileprivate var right: BVHNode {
-        return BVHNode(index: 2 * self.index + 2, bvh: self.bvh)
-    }
-
-    fileprivate func visit(callback: (BVHNode, Int) -> Bool) {
-        self.visit(callback: callback, currentDepth: 0)
-    }
-        
-    // MARK: - private
-    
-    private func visit(callback: (BVHNode, Int) -> Bool, currentDepth depth: Int) {
-        guard callback(self, depth) == true else {
-            return
-        }
-        if case .`internal` = self.nodeType {
-            self.left.visit(callback: callback, currentDepth: depth+1)
-            self.right.visit(callback: callback, currentDepth: depth+1)
-        }
+    fileprivate init(boundingBox: BoundingBox, type: NodeType) {
+        self.nodeType = type
+        self.boundingBox = boundingBox
     }
 }
