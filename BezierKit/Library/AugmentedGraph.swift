a49ba0970d2a2dd2740f2ee9365266227ba205f0
Holmes Futrell <holmes@gingerlabs.com>
2019-06-04 09:12:47 -0700

    Revert "checking in work, tests pass but the underlying issue is probably still there."
    This reverts commit f728a9a7db6410f4d3f7d7f3290afc68a89c1b41.

--- BezierKit/Library/AugmentedGraph.swift
+++ BezierKit/Library/AugmentedGraph.swift
@@ -196,10 +196,11 @@ internal class PathLinkedListRepresentation {
                 let previous = v.emitPrevious()
                 let next = v.emitNext()
 
-                let n1 = neighbor.emitPrevious().approximateDerivative(0)
-                let n2 = neighbor.emitNext().approximateDerivative(0)
-                let v1 = previous.approximateDerivative(0)
-                let v2 = next.approximateDerivative(0)
+                let smallNumber: CGFloat = 0.001
+                let n1 = neighbor.emitPrevious().compute(smallNumber) - v.location
+                let n2 = neighbor.emitNext().compute(smallNumber) - v.location
+                let v1 = previous.compute(smallNumber) - v.location
+                let v2 = next.compute(smallNumber) - v.location
 
                 let windingCountChange = windingCountAdjustment(v1, v2, n1, n2)
 
