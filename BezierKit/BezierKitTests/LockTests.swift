151f0d21b6059d76fe057fae09466a27cff0744b
Holmes Futrell <holmes@gingerlabs.com>
2020-10-14 16:14:12 -0700

    lock tests should run on Linux.

--- BezierKit/BezierKitTests/LockTests.swift
+++ BezierKit/BezierKitTests/LockTests.swift
@@ -12,23 +12,29 @@ import XCTest
 class LockTests: XCTestCase {
     func testPathPropertyAtomicity() {
         // ensure that lazy properties of Path are only initialized once
-        let squareCGPath = CGPath(rect: CGRect(x: 0, y: 0, width: 1, height: 1), transform: nil)
-        let path = Path(cgPath: squareCGPath)
+        let rect = CGRect(x: 0, y: 0, width: 1, height: 1)
+        let path = Path(rect: rect)
 
         let threadCount = 10000
         let expectation = XCTestExpectation()
         expectation.expectedFulfillmentCount = threadCount
 
+    #if canImport(CoreGraphics)
         var cgPaths: [Int: CGPath] = [:]
+    #endif
         var boundingBoxes: [Int: BoundingBox] = [:]
 
         for i in 0..<threadCount {
             let index = i
             DispatchQueue.global(qos: .default).async {
+                #if canImport(CoreGraphics)
                 let pathValue = path.cgPath
+                #endif
                 let boundingBoxValue = path.boundingBox
                 DispatchQueue.main.async {
+                #if canImport(CoreGraphics)
                     cgPaths[index] = pathValue
+                #endif
                     boundingBoxes[index] = boundingBoxValue
                     expectation.fulfill()
                 }
@@ -36,11 +42,13 @@ class LockTests: XCTestCase {
         }
         wait(for: [expectation], timeout: 10.0)
 
+        #if canImport(CoreGraphics)
         XCTAssertEqual(cgPaths.values.count, threadCount)
-        XCTAssertEqual(cgPaths[0], Path(cgPath: squareCGPath).cgPath)
+        XCTAssertEqual(cgPaths[0], Path(rect: rect).cgPath)
         XCTAssertTrue(cgPaths.values.allSatisfy { $0 === cgPaths[0] }, "cgPaths should all refer to the same instance (was it initialized more than once?)")
+        #endif
 
-        let expectedBoundingBox = Path(cgPath: squareCGPath).boundingBox
+        let expectedBoundingBox = Path(rect: rect).boundingBox
         XCTAssertEqual(boundingBoxes.values.count, threadCount)
         XCTAssertTrue(boundingBoxes.values.allSatisfy { $0 == expectedBoundingBox })
     }
