995300e3f75a4f9b5efc94bb5cd78cc3948a29c8
Holmes Futrell <holmes@gingerlabs.com>
2020-10-15 15:35:12 -0700

    try to break apart travis into script files with support for multiple platforms.

---
+++ travis/script.sh
@@ -0,0 +1,17 @@
+if [[ $TRAVIS_OS_NAME = 'osx' ]]; then
+	set -o pipefail
+	xcodebuild -version
+	xcodebuild -showsdks
+	# Build Framework in Debug and Run Tests if specified
+	if [ $RUN_TESTS == "YES" ]; then
+		xcodebuild -project "$PROJECT" -scheme "$SCHEME" -sdk "$SDK" -destination "$DESTINATION" -configuration Debug ONLY_ACTIVE_ARCH=NO test | xcpretty -c;
+	else
+		xcodebuild -project "$PROJECT" -scheme "$SCHEME" -sdk "$SDK" -destination "$DESTINATION" -configuration Debug ONLY_ACTIVE_ARCH=NO build | xcpretty -c;
+	fi
+  	# Run `pod spec lint` if specified
+  	if [ $POD_LINT == "YES" ]; then
+  		pod spec lint;
+    fi
+elif [[ $TRAVIS_OS_NAME = 'linux' ]]; then
+	swift test --enable-test-discovery
+fi
