4a327c1d4364b63cce4ee5b198df4b7a09c7f8a9
hfutrell <holmes@gingerlabs.com>
2020-11-25 13:58:26 -0800

    performance test for project.

--- BezierKit/BezierKitTests/PathComponent+ProjectionTests.swift
+++ BezierKit/BezierKitTests/PathComponent+ProjectionTests.swift
@@ -12,7 +12,6 @@ import XCTest
 
 class PathComponentProjectionTests: XCTestCase {
     func testProject() {
-        
         let line = PathComponent(curve: LineSegment(p0: CGPoint(x: 1, y: 1), p1: CGPoint(x: 2, y: 2)))
         let point1 = CGPoint(x: 1, y: 2)
         let result1 = line.project(point1)
@@ -32,5 +31,65 @@ class PathComponentProjectionTests: XCTestCase {
         XCTAssertTrue(component.pointIsWithinDistanceOfBoundary(point2, distance: 10))
         XCTAssertTrue(component.pointIsWithinDistanceOfBoundary(point2, distance: 2))
         XCTAssertFalse(component.pointIsWithinDistanceOfBoundary(point2, distance: 0.5))
+    }
+
+    func parametricPath(numCurves: Int,
+                        theta: (_: CGFloat) -> CGFloat,
+                        dthetadt: (_: CGFloat) -> CGFloat,
+                        r: (_: CGFloat) -> CGFloat,
+                        drdt: (_: CGFloat) -> CGFloat) -> Path {
+        func p(_ t: CGFloat) -> CGPoint {
+            return CGPoint(x: r(t) * cos(theta(t)), y: r(t) * sin(theta(t)))
+        }
+        func d(_ t: CGFloat) -> CGPoint {
+            return CGPoint(x: drdt(t) * cos(theta(t)) - r(t) * sin(theta(t)) * dthetadt(t),
+                           y: drdt(t) * sin(theta(t)) + r(t) * cos(theta(t)) * dthetadt(t))
+        }
+        let cgPath = CGMutablePath()
+        var previousT: CGFloat = 0.0
+        var previousPoint = p(previousT)
+        cgPath.move(to: previousPoint)
+        let delta = 1.0 / CGFloat(numCurves)
+        for i in 1...numCurves {
+            let nextT = CGFloat(i) / CGFloat(numCurves)
+            let nextPoint = p(nextT)
+            cgPath.addCurve(to: nextPoint, control1: previousPoint + delta / 3.0 * d(previousT), control2: nextPoint - delta / 3.0 * d(nextT))
+            previousPoint = nextPoint
+            previousT = nextT
+        }
+        return Path(cgPath: cgPath)
+    }
+
+    func testProjectPerformance() {
+        let k: CGFloat = 2.0 * CGFloat.pi * 10
+        let maxRadius: CGFloat = 100.0
+        func theta(_ t: CGFloat) -> CGFloat {
+            return k * t
+        }
+        func r(_ t: CGFloat) -> CGFloat {
+            return t * maxRadius
+        }
+        func drdt(_ t: CGFloat) -> CGFloat {
+            return maxRadius
+        }
+        func dthetadt(_ t: CGFloat) -> CGFloat {
+            return k
+        }
+        let spiral = parametricPath(numCurves: 100, theta: theta, dthetadt: dthetadt, r: r, drdt: drdt)
+        // about 0.31s in -Onone, 0.033s in -Ospeed
+        self.measure {
+            var pointsTested = 0
+            var totalDistance: CGFloat = 0.0
+            for x in stride(from: -maxRadius, through: maxRadius, by: 10) {
+                for y in stride(from: -maxRadius, through: maxRadius, by: 10) {
+                   // print("(\(x), \(y))")
+                    let point = CGPoint(x: x, y: y)
+                    let projection = spiral.project(point)!.point
+                    pointsTested += 1
+                    totalDistance += distance(projection, point)
+                }
+            }
+            // print("tested \(pointsTested) points, average distance from spiral = \(totalDistance / CGFloat(pointsTested))")
+        }
     }
 }
