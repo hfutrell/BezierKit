110bc591f4443ea3e0c4d3bec817135ea16c2d98
hfutrell <holmes@gingerlabs.com>
2024-10-18 15:38:19 -0700

    strict concurrency checking.

--- BezierKit/BezierKitTests/LockTests.swift
+++ BezierKit/BezierKitTests/LockTests.swift
@@ -6,12 +6,26 @@
 //  Copyright © 2019 Holmes Futrell. All rights reserved.
 //
 
-import XCTest
+@preconcurrency import XCTest
 @testable import BezierKit
 
 #if !os(WASI)
 class LockTests: XCTestCase {
-    func testPathPropertyAtomicity() {
+    func testPathPropertyAtomicity() async {
+
+        @MainActor class Results: Sendable {
+            #if canImport(CoreGraphics)
+            var cgPaths: [Int: CGPath] = [:]
+            func setPath(at index: Int, _ value: CGPath) {
+                cgPaths[index] = value
+            }
+            #endif
+            var boundingBoxes: [Int: BoundingBox] = [:]
+            func setBoundingBox(at index: Int, _ value: BoundingBox) {
+                boundingBoxes[index] = value
+            }
+        }
+
         // ensure that lazy properties of Path are only initialized once
         let rect = CGRect(x: 0, y: 0, width: 1, height: 1)
         let path = Path(rect: rect)
@@ -20,38 +34,36 @@ class LockTests: XCTestCase {
         let expectation = XCTestExpectation()
         expectation.expectedFulfillmentCount = threadCount
 
-    #if canImport(CoreGraphics)
-        var cgPaths: [Int: CGPath] = [:]
-    #endif
-        var boundingBoxes: [Int: BoundingBox] = [:]
-
+        let results = await Results()
         for i in 0..<threadCount {
             let index = i
-            DispatchQueue.global(qos: .default).async {
-                #if canImport(CoreGraphics)
+            Task.detached {
+#if canImport(CoreGraphics)
                 let pathValue = path.cgPath
-                #endif
+#endif
                 let boundingBoxValue = path.boundingBox
-                DispatchQueue.main.async {
-                #if canImport(CoreGraphics)
-                    cgPaths[index] = pathValue
-                #endif
-                    boundingBoxes[index] = boundingBoxValue
+                await MainActor.run {
+#if canImport(CoreGraphics)
+                    results.setPath(at: index, pathValue)
+#endif
+                    results.setBoundingBox(at: index, boundingBoxValue)
                     expectation.fulfill()
                 }
             }
         }
-        wait(for: [expectation], timeout: 10.0)
 
-        #if canImport(CoreGraphics)
-        XCTAssertEqual(cgPaths.values.count, threadCount)
-        XCTAssertEqual(cgPaths[0], Path(rect: rect).cgPath)
-        XCTAssertTrue(cgPaths.values.allSatisfy { $0 === cgPaths[0] }, "cgPaths should all refer to the same instance (was it initialized more than once?)")
-        #endif
+        await fulfillment(of: [expectation], timeout: 10.0)
 
-        let expectedBoundingBox = Path(rect: rect).boundingBox
-        XCTAssertEqual(boundingBoxes.values.count, threadCount)
-        XCTAssertTrue(boundingBoxes.values.allSatisfy { $0 == expectedBoundingBox })
+        await MainActor.run {
+#if canImport(CoreGraphics)
+            XCTAssertEqual(results.cgPaths.values.count, threadCount)
+            XCTAssertEqual(results.cgPaths[0], Path(rect: rect).cgPath)
+            XCTAssertTrue(results.cgPaths.values.allSatisfy { $0 === results.cgPaths[0] }, "cgPaths should all refer to the same instance (was it initialized more than once?)")
+#endif
+            let expectedBoundingBox = Path(rect: rect).boundingBox
+            XCTAssertEqual(results.boundingBoxes.values.count, threadCount)
+            XCTAssertTrue(results.boundingBoxes.values.allSatisfy { $0 == expectedBoundingBox })
+        }
     }
 }
 #endif
